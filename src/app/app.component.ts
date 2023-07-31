207dc06472353a7684cf8f9b3b4e59fb62da0d4a
Esmyrna <esmyrnaoliveira13@gmail.com>
2023-07-31 09:45:54 -0300

    criando funções para a aplicação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,10 +19,22 @@ export class AppComponent {
         Validators.maxLength(60),
         Validators.required
       ])]
-    })
-    this.todos.push(new Todo(1, 'Passear com o cachorro', true));
-    this.todos.push(new Todo(2, 'Dormir', false));
-    this.todos.push(new Todo(3, 'Comer', true));
+    });
+
+    this.load();
+  }
+
+  add(){
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length + 1;
+    this.todos.push(new Todo(id, title, false));
+    this.save();
+    this.clear();
+
+  }
+
+  clear() {
+    this.form.reset();
   }
 
   alterarTexto(){
@@ -34,13 +46,30 @@ export class AppComponent {
      if(index !== -1) {
          this.todos.splice(index, 1)
      }
+     this.save();
   }
 
   markAsDone(todo: Todo) {
-      todo.done = true
+      todo.done = true;
+      this.save();
   }
 
   markAsUndone(todo: Todo) {
-      todo.done = false
+      todo.done = false;
+      this.save();
+  }
+
+  save() {
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
+  }
+
+  load() {
+    const data = localStorage.getItem('todos');
+    if(data !== null) {
+        this.todos = JSON.parse(data);
+    }else{
+      this.todos = [];
+    }   
   }
 }
